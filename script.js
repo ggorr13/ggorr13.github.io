a85988619ae63ccbd9d1f7f3401aaee33c1f3c6c
Gor Tamazyan <tamazyangor7@gmail.com>
2023-06-02 15:50:19 +0400

    update

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Wrap every letter in a span
 
-anime.timeline({loop: true})
-    .add({
-        targets: '.ml12',
-        translateX: [40,0],
-        translateZ: 0,
-        opacity: [0,1],
-        easing: "easeOutExpo",
-        duration: 10000,
-        delay: (el, i) => 500 + 30 * i
-    }).add({
-    targets: '.ml12',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 30000,
-    delay: (el, i) => 100 + 30 * i
-});
+// anime.timeline({loop: true})
+//     .add({
+//         targets: '.ml12',
+//         translateX: [40,0],
+//         translateZ: 0,
+//         opacity: [0,1],
+//         easing: "easeOutExpo",
+//         duration: 1000,
+//         delay: (el, i) => 500 + 30 * i
+//     }).add({
+//     targets: '.ml12',
+//     translateX: [0,-30],
+//     opacity: [1,0],
+//     easing: "easeInExpo",
+//     duration: 10000,
+//     delay: (el, i) => 100 + 30 * i
+// });
